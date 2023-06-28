bd073bc48b0e358927541cb5780f189bf91372a6
Juanelas <j.hernandez@upc.edu>
2023-06-28 16:47:52 +0200

    ready to merge

--- src/ts/phi.ts
+++ src/ts/phi.ts
@@ -3,7 +3,7 @@ export type PrimeFactorization = Array<[bigint, bigint]>
 /**
  * A function that computes the Euler's totien function of a number n, whose prime power factorization is known
  *
- * @param primeFactorization an array of arrays containing the prime power factorization, i.e. for n = (p1**k1)*(p2**k2)*...*(pr**kr), one should provide [[p1, k1], [p2, k2], ... , [pr, kr]]
+ * @param primeFactorization an array of arrays containing the prime power factorization of a number n. For example, for n = (p1**k1)*(p2**k2)*...*(pr**kr), one should provide [[p1, k1], [p2, k2], ... , [pr, kr]]
  * @returns phi((p1**k1)*(p2**k2)*...*(pr**kr))
  */
 export function phi (primeFactorization: PrimeFactorization): bigint {
